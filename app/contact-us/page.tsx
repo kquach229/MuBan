8dcd409fa399e56ba4a7ca80e54bdc360f645cec
Kenny Quach <kquach229@gmail.com>
2024-12-30 22:27:33 -0500

    favicon and other improvements

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,13 +1,15 @@
 import ContactForm from "@/components/ContactForm";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { div } from "motion/react-client";
 import React from "react";
+import { FaBusinessTime } from "react-icons/fa";
 
 const ContactUs = () => {
   return (
-    <div className="min-h-screen flex justify-around mt-[15rem] p-[2rem]">
-      <Card className="w-[40%] p-10 max-h-[500px]">
-        <div className="font-extrabold text-3xl">Get in Touch.</div>
-        <div className="mt-5 [&>*]:mt-3">
+    <div className="flex min-h-screen flex-col gap-20 md:justify-around md:flex-row mt-[15rem] ">
+      <Card className="w-full md:w-[40%] p-10 max-h-[500px]">
+        <div className="font-extrabold text-3xl mt-[5rem]">Get in Touch.</div>
+        <div className="[&>*]:mt-5">
           <p>
             Have questions, need support, or want to learn more about our
             templates? We're here to help!
@@ -22,11 +24,14 @@ const ContactUs = () => {
           </p>
         </div>
       </Card>
-      <Card className="w-[40%] p-10 max-h-[500px]">
-        <span className="mx-auto w-full text-right">
-          <p className="font-bold">Hours of Operation</p>
-          <p>Mon - Fri: 8AM - 8PM</p>
-          <p>Sat and Sun: 10AM-11PM</p>
+      <Card className="w-full md:w-[40%] p-10 max-h-[500px]">
+        <span className="mx-auto w-full text-right flex items-center h-full justify-between">
+          <FaBusinessTime size={50} />
+          <div className="flex-1">
+            <p className="font-bold">Hours of Operation</p>
+            <p>Mon - Fri: 8AM - 8PM</p>
+            <p>Sat and Sun: 10AM-11PM</p>
+          </div>
         </span>
       </Card>
     </div>
