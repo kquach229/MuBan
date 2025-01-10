8b4f43d3172ced5ab75596fb59252e5e42bfa719
kquach229 <kquach229@gmail.com>
2025-01-10 17:09:27 -0500

    fix prisma issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,28 @@
-import CategoriesGrid from '@/components/CategoriesGrid';
 import Hero from '@/components/Hero';
 import { prisma } from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server';
 export default async function Home() {
-  const categories = [
-    { id: 1, category: 'Portfolio & Agency' },
-    { id: 2, category: 'Technology' },
-    { id: 3, category: 'Blog & Tutorial' },
-    { id: 4, category: 'Professional Services' },
-    { id: 5, category: 'Real Estate' },
-    { id: 7, category: 'Retail' },
-  ];
-
   const user = await currentUser();
 
-  const loggedInUser = await prisma.user.findUnique({
-    where: {
-      email: user?.emailAddresses[0]?.emailAddress,
-    },
-  });
-
-  if (!loggedInUser) {
-    await prisma.user.create({
-      data: {
-        name: `${user?.firstName} ${user?.lastName}`,
-        email: user?.emailAddresses[0]?.emailAddress,
-        purchasedTemplates: {},
+  // only execute if user is signed in
+  if (user !== null) {
+    // find whether or not current signed in user exists in prisma
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: user?.emailAddresses[0].emailAddress,
       },
     });
+
+    // if not, then create a user record in prisma
+    if (!existingUser) {
+      await prisma.user.create({
+        data: {
+          name: `${user?.firstName} ${user?.lastName}`,
+          email: user?.emailAddresses[0]?.emailAddress,
+          purchasedTemplates: {},
+        },
+      });
+    }
   }
 
   return (
